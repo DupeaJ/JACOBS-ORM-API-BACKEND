dc59147f8ce9081c0fb3949cfb82ecc9d518c6a7
unknown <jacobdupea@gmail.com>
2023-10-18 17:10:08 -0700

    fixed category and product routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+    foreignKey: "categoryId",
+    as: "category",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+    foreignKey: "categoryId",
+    as: "products",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: "tags",
+    foreignKey: "productId",
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: "products",
+    foreignKey: "tagId",
+});
 
 module.exports = {
   Product,
